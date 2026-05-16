cff11c5e14e605ccba689834174059ce2e8fd4ac
EsanduEpa <esanduepa0225@gmail.com>
2026-05-16 21:32:57 +0530

    Added Gemini Ai and test it

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+const { GoogleGenAI } = require("@google/genai");
+
+// Log the status of the Gemini API key to ensure it's loaded correctly
+console.log("GEMINI_API_KEY:", process.env.GEMINI_API_KEY ? "Loaded" : "Not Loaded");
 
 const app = express();
+
+// create ai object to interact with Gemini API
+const ai = new GoogleGenAI({
+    apiKey: process.env.GEMINI_API_KEY,
+});
 
 app.use(cors());
 app.use(express.json());
@@ -10,6 +21,7 @@ app.get("/", (req, res) => {
   res.send("Backend is running");
 });
 
+// get from summarize endpoint and return a short summary of the notes
 app.post("/summarize", (req, res) => {
     const notes = req.body.notes;
     console.log("Received notes:", notes);
@@ -19,6 +31,27 @@ app.post("/summarize", (req, res) => {
         summary: shortSummary + "..."
     });
     })
+
+
+
+
+app.get("/test-ai", async (req, res) => {
+  try {
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: "who is the largest fish",
+    });
+
+    res.json({
+      message: response.text,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: "AI test failed",
+    });
+  }
+});
 
 app.listen(5001, () => {
   console.log("Server running on http://localhost:5001");
